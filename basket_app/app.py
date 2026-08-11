0087623dd787e2539505c405f9efcc5f1b817bb0
anastasiavg2002-cyber <anastasiavg2002@gmail.com>
2026-08-11 12:56:17 +0300

    Update app.py

--- basket_app/app.py
+++ basket_app/app.py
@@ -208,7 +208,7 @@ if total_file:
         st.session_state.mapping,
         use_container_width=True
     )
-     st.info(
+    st.info(
     "Проверьте, что названия в правых столбцах в файле Total и  совпадают"
     st.session_state.mapping = generate_mapping1(
         total_file
